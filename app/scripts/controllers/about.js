1757bae5a33f1ae28e1cc886570c03a3e09a757b
Kimmyrae <kimberly.mcbain@gmail.com>
2015-07-22 14:07:35 -0700

    added files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularAppApp
+ */
+angular.module('angularAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
