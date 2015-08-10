f8ca2fc1b0847f463d67f3564929af01d2feb43e
Kimmyrae <kimberly.mcbain@gmail.com>
2015-08-10 16:48:58 -0700

    added button

--- app/scripts/services/recipesearch.js
+++ app/scripts/services/recipesearch.js
@@ -8,16 +8,24 @@
  * Factory in the angularAppApp.
  */
 angular.module('angularAppApp')
-  .factory('recipesearch', function () {
+  .factory('recipesearch', function ($resource) {
     // Service logic
     // ...
 
     var meaningOfLife = 42;
 
     // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
+    return $resource('http://api.yummly.com/v1/api/recipes?_app_id=3ca00079&_app_key=51e01589489c5872d2b13c715aba12d8&q=plantain&requirePictures=true', {}, {
+      query: {
+        method:'GET',
+        isArray:false,
+        params: {
+          q: "plantain"
+        },
+        headers: {
+          'X-Yummly-App-ID': '3ca00079',
+          'X-Yummly-App-Key': '51e01589489c5872d2b13c715aba12d8'
+        }
       }
-    };
+    });
   });
