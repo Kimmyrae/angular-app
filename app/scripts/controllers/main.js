ad8caf1c6a3d5bb5bbe29d1392cf9a1e93cd0ae3
Kimmyrae <kimberly.mcbain@gmail.com>
2015-08-12 15:07:46 -0700

    changed a bunch of stuff
    Halp!!!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,8 +7,18 @@
  * # MainCtrl
  * Controller of the angularAppApp
  */
-  angular.module('angularAppApp')
-    .controller('MainCtrl', function ($scope, current) {
-      $scope.current = current.query();
-      $scope.test = "test";
+angular.module('angularAppApp')
+  .controller('MainCtrl', function ($scope, recipesearch) {
+    $scope.recipesFound = recipesearch.find();
+    $scope.recipesFound.$promise.then(function(data){
+      $scope.recipesreal=data;
+      console.log(data);
     });
+
+    $scope.findRecipes= function(){
+        $scope.recipesFound = recipesearch.find({
+            query: $scope.ingredients
+        });
+        $scope.searchQuery = $scope.recipeName;
+    };
+  });
--- app/scripts/services/recipesearch.js
+++ app/scripts/services/recipesearch.js
@@ -12,11 +12,10 @@ angular.module('angularAppApp')
     // Service logic
     // ...
 
-    var meaningOfLife = 42;
 
     // Public API here
-    return $resource('http://api.yummly.com/v1/api/recipes?_app_id=3ca00079&_app_key=51e01589489c5872d2b13c715aba12d8&q=plantain&requirePictures=true', {}, {
-      query: {
+    return $resource('http://api.yummly.com/v1/api/recipes?_app_id=3ca00079&_app_key=51e01589489c5872d2b13c715aba12d8&q=:query&requirePictures=true', {}, {
+      find: {
         method:'GET',
         isArray:false,
         params: {
