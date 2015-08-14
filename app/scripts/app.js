7f32b851bde5495fa69170ba4c5d0bae6a2fae37
Kimmyrae <kimberly.mcbain@gmail.com>
2015-08-13 19:58:50 -0700

    added localstorage

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,6 +17,7 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
+    'ngStorage', //added to enable localStorage features
     'ngTouch'
   ])
   .config(function ($routeProvider) {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,25 @@
  * Controller of the angularAppApp
  */
 angular.module('angularAppApp')
-  .controller('MainCtrl', function ($scope, recipesearch) {
+  .controller('MainCtrl', function ($scope, recipesearch, $localStorage) {
     $scope.recipesFound = recipesearch.find();
     // $scope.recipesFound.$promise.then(function(data){
     //   $scope.recipesreal=data;
     //   console.log(data);
     // });
-
+    $scope.savedRecipes=localStorage.savedRecipes;
     $scope.findRecipes= function(){
         $scope.recipesFound = recipesearch.find({
             query: $scope.query
+
         });
-        $scope.searchQuery = $scope.recipeName;
     };
-  });
+
+    $scope.saveRecipe = function(recipe){
+      if ($localStorage.savedRecipes){
+        $localStorage.savedRecipes.push(recipe);
+      }  else {
+        $localStorage.savedRecipes=[recipe];
+      }
+    };
+});
